6f4247b6087fd57a6c216d602e389b7ee0d326d8
최유진 <youjin.choi@navercorp.com>
2017-11-17 01:30:30 +0900

    Restful API 적용, swagger api-docs 적용

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var path = require('path');
-var querystring = require('querystring'); 
 var express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('mongoose');
+// for dev
+//mongoose.connect('mongodb://10.106.144.145:27017/test');
+// for real
 mongoose.connect('mongodb://localhost:27017/test');
 var db = mongoose.connection;
 db.on('error', function(){
@@ -13,24 +16,31 @@ db.once('open', function() {
     console.log('MongoDB Connected!');
 });
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/questions', function(req, res) {
-    var questions = db.collection('test').find().toArray(function(error, data){
+    db.collection('test').find().toArray(function(error, data){
+            var apiResponse = {
+                status: "OK",
+                result: data
+            };
             res.writeHead(200, {'Content-Type':'application/json;charset=UTF-8'});
-            res.end(JSON.stringify(data));
+            res.end(JSON.stringify(apiResponse));
         });      
 });
 
 app.post('/questions', function(req, res){
     var body = req.body;
     db.collection('test').insert({questioner:body.questioner, contents:body.contents, options:body.options});
+    var apiResponse = {
+        status: "OK"
+    };
     res.writeHead(200, {'Content-Type':'application/json'});
-    res.end('{"status":"OK"}');
+    res.end(JSON.stringify(apiResponse));
 });
 
-app.use('/swagger-ui', express.static(path.join(__dirname, './node_modules/swagger-ui/dist')));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(3000, function () { 
-    console.log('Example app listening on port 3000!');
+app.listen(8080, function () { 
+    console.log('Wepick app listening on port 8080!');
 });
