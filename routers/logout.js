222c70bd24a69b9d666c8a70e4da79eb2b86f6f7
최유진 <youjin.choi@navercorp.com>
2017-12-07 19:21:28 +0900

    로그아웃 api 추가

---
+++ routers/logout.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+var Question = require('../models/question');
+var Answer = require('../models/answer');
+var commonResponse = require('../commons/commonResponse');
+var NoAccessKeyError = require('../errors/NoAccessKeyError');
+var NoUserError = require('../errors/NoUserError');
+
+var GUEST = 1;
+var MEMBER = 2;
+
+router.post('/', function(req, res, next) {
+	var accessKey = req.get('Access-Key');
+	if (!accessKey) {
+		throw NoAccessKeyError();
+	}
+	User.findOne({'email': body.email})
+	.then(user => {
+		if (!user) {
+			throw NoUserError();
+		}
+		
+		// 일반 유저 로그아웃시 푸시 토큰 삭제
+		if (user.type == MEMBER) {
+			user.pushToken = null;
+			user.save().then(() => commonResponse.ok(res));
+		} else if (user.type == GUEST) {	// 게스트 유저 로그아웃시 탈퇴처리
+			user.remove()
+			.then(() => {
+				return Question.remove({'questioner': user.seq});
+			})
+			.then(() => {
+				return Answer.remove({'questioner': user.seq});
+			})
+			.then(() => {
+				commonResponse.ok(res);
+			});
+		}
+	})
+	.catch(next);
+});
+
+module.exports = router;
