da3f92a04396bc2a234d5a9d7a06b85873aff8ca
youijchoi <dev.youjin.choi@gmail.com>
2017-12-25 03:14:46 +0900

    배포 스크립트 개선

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,16 +21,27 @@ if [ "$CURRENT_NODE_PORT" = '8080' ]
 then
 	NEW_NODE_PORT=8081
 else
-	NEW_NODE_PORT=8080
+   	NEW_NODE_PORT=8080
 fi
 
 OLD_NODE_PORT=$CURRENT_NODE_PORT
 
 $SCRIPTS_DIR/server_start.sh $NEW_NODE_PORT
+echo "Node start end."
+
+sleep 3
+
 sudo rm $NGINX_CONF_DIR/conf.d/wepick.conf
 sudo ln -s $NGINX_CONF_DIR/conf.d/wepick.$NEW_NODE_PORT.conf $NGINX_CONF_DIR/conf.d/wepick.conf
+echo "Nginx config change end."
+
+sleep 3
+
 sudo systemctl reload nginx
 echo $NEW_NODE_PORT > ~/$NODE_PORT_FILE
+echo "Nginx reload end."
+
+sleep 3
 
 OLD_NODE_PID=`ps -ef | grep node | grep $OLD_NODE_PORT | awk '{print $2}'`
 kill -9 $OLD_NODE_PID
